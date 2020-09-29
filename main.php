d439c9a15c33c41bb3b019115b2a3c73b49c9cc7
Riccardo Colombo <rikyregna@gmail.com>
2020-09-29 15:53:51 +0200

    setting laraver + include header

---
+++ main.php
@@ -0,0 +1,36 @@
+<?php 
+
+	/* includo l'header prendendolo da templates */
+	include __DIR__ . '/partials/templates/header.php';
+
+?>
+
+<body>
+
+<div class="container content">
+<table class="table">
+	<thead>
+		<tr>
+			<th>ID: </th>
+			<th>Numero stanza: </th>
+			<th>Numero piano: </th>
+			<th></th>
+			<th></th>
+			<th></th>
+		</tr>
+	</thead>
+	<tbody>
+		<tr>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+		</tr>
+	</tbody>
+</table>
+</div>
+	
+</body>
+</html>
